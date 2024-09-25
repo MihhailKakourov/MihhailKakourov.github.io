f2afd341f13d7b27c6f24802774660f0f6140ddd
Daniel <mihhail.kakourov@ivkhk.ee>
2024-09-25 15:18:14 +0300

    part 3 started

--- webapp/js/script.js
+++ webapp/js/script.js
@@ -169,6 +169,7 @@ class MenuCard{
         `;
         this.parent.append(element);
     }
+
 }
 
 new MenuCard(
@@ -202,6 +203,13 @@ new MenuCard(
     ".menu .container"
 ).render();
 
+getResource("http://localhost:3000/menu")
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        })
+
 const formData = document.querySelectorAll("form");
 const message = {
     loading : 'img/form/spinner.svg',
@@ -213,32 +221,44 @@ forms.forEach(item => {
     postData(item);
 });
 
-function postData(form){
-    form.addEventListener('submit', (e) => {
+const postData = async(url, data) => {
+    let res = await fetch(url, {
+        method : 'POST',
+        headers : {
+            "content-type": "application/json"
+        },
+        body: data
+    })
+    return await res.json();
+};
+
+async function getResource(url) {
+    let res = await fetch(url);
+
+    if (!res.ok){
+        throw new Error(`Error fetching resource: ${url}, response: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
+function bindPostData(form){
+    form.addEventListener("submit", (e) =>{
         e.preventDefault();
 
-        let statusMessage = document.createElement('img');
+        let statusMessage = document.createElement("img");
         statusMessage.src = message.loading;
-        statusMessage.style.cssText = `
+            statusMessage.style.cssText = `
             display: block;
             margin: 0 auto;
-        `;
-        form.insertAdjacentElement('afterend', statusMessage);
+            `;
+        form.insertAdjacentElement("afterend", statusMessage);
 
         const formData = new FormData(form);
+        const json = JSON.stringify(Object.fromEntries(formData.entries));
 
-        const object = {};
-        formData.forEach(function (value, key) {
-            object[key] = value;
-        });
-
-        fetch('server.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(object)
-        }).then(data => {
+        postData("http://localhost:3000/request", json)
+        .then(data => {
             console.log(data);
             showThanksModal(message.success);
             statusMessage.remove();
@@ -246,9 +266,47 @@ function postData(form){
             showThanksModal(message.failure);
         }).finally(() => {
             form.reset();
-        });
-    });
+        })
+    })
 }
+
+// function postData(form){
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+
+//         let statusMessage = document.createElement('img');
+//         statusMessage.src = message.loading;
+//         statusMessage.style.cssText = `
+//             display: block;
+//             margin: 0 auto;
+//         `;
+//         form.insertAdjacentElement('afterend', statusMessage);
+
+//         const formData = new FormData(form);
+
+//         const object = {};
+//         formData.forEach(function (value, key) {
+//             object[key] = value;
+//         });
+
+//         fetch('server.php', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(object)
+//         }).then(data => {
+//             console.log(data);
+//             showThanksModal(message.success);
+//             statusMessage.remove();
+//         }).catch(() => {
+//             showThanksModal(message.failure);
+//         }).finally(() => {
+//             form.reset();
+//         });
+//     });
+// }
+
 function showThanksModal(message) {
     const prevModalDialog = document.querySelector('.modal__dialog');
 
@@ -271,3 +329,131 @@ function showThanksModal(message) {
         closeModal();
     }, 4000);
 }
+
+let offset = 0;
+let slideIndex = 0;
+
+const slides = document.querySelectorAll('.offer__slide'),
+    slider = document.querySelector(".offer__slider"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    total = document.querySelector("#total"),
+    current = document.querySelector("#current"),
+    slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+    width = window.getComputedStyle(slidesWrapper).width,
+    slidesField = document.querySelector(".offer__slides-inner");
+
+    if(slides.length < 10) {
+        total.textContent = `0${slides.length}`
+        current.textContent = `0${slideIndex}`
+    }else{
+        total.textContent = slides.length;
+        current.textContent = slideIndex
+    }
+
+    slidesField.style.width = 100*slides.length + '%';
+    slidesField.style.display = "flex";
+    slidesField.style.transition = "0.5s all";
+
+    slidesWrapper.style.overflow = "hidden";
+
+    slides.forEach(slide => {
+        slide.style.width = width
+    })
+
+    slider.style.position = "relative";
+
+    const indicators = document.createElement("ol"),
+        dots = [];
+    indicators.classList.add("carousel-indicators");
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-left: 15%;
+        margin-right: 15%;
+        list-style: none;
+    `
+    slider.append(indicators);
+
+    for(let i = 0; i < slides.length; i++) {
+        const dot = document.createElement("li");
+        dot.setAttribute("data-slide-to", i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0){
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    next.addEventListener('click', () => {
+        if (offset == (deleteNotDigits(width) * (slides.length))){
+            offset = 0
+        }else{
+            offset += deleteNotDigits(width);
+        }
+        slidesField.style.transform = `translateX(${offset}px)`;
+
+        if(slideIndex == slides.lenght){
+            slideIndex = 1
+        } else{
+            slideIndex++;
+        }
+        if (slides.lenght <10) {
+            current.textContent = `0${slideIndex}`;
+        }else{
+            current.textContent = slideIndex;
+        }
+        dots.forEach(dot => dot.style.opacity = "0.5");
+        dots[slideIndex].style.opacity = 1;
+    });
+
+    prev.addEventListener("click", () => {
+        if (offset == 0){
+            offset = deleteNotDigits(width) * (slides.length - 1);
+        }else{
+            offset -= deleteNotDigits(width)
+        }
+
+        slidesField.style.transform = `translateX(${offset}px)`;
+
+        if(slideIndex == 1){
+            slideIndex = slides.length;
+        }else{
+            slideIndex--;
+        }
+
+        if(slides.length<10){
+            current.textContent = `0${slideIndex}`;
+        }else{
+            current.textContent = slideIndex
+        }
+
+        dots.forEach(dot => dot.style.opacity = 0.5);
+        dots[slideIndex-1].style.opacity = 1;
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) =>{
+
+        })
+    })
