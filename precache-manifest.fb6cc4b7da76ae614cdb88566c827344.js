80f3e8424f33dc15d7ee435a8136a3c164f793a6
Daniel <danielkisseljov96@gmail.com>
2024-10-10 15:08:41 +0300

    Updates

---
+++ precache-manifest.fb6cc4b7da76ae614cdb88566c827344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2fc2fe133b6988199d7",
+    "url": "/React/static/css/main.c289dddb.chunk.css"
+  },
+  {
+    "revision": "f2fc2fe133b6988199d7",
+    "url": "/React/static/js/main.f2fc2fe1.chunk.js"
+  },
+  {
+    "revision": "b4b74d0c6a819172b915",
+    "url": "/React/static/css/1.ee8f18eb.chunk.css"
+  },
+  {
+    "revision": "b4b74d0c6a819172b915",
+    "url": "/React/static/js/1.b4b74d0c.chunk.js"
+  },
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "680169dfd33066b4c9d8ebe91dbae5b6",
+    "url": "/React/index.html"
+  }
+];
